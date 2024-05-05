852a1975e5d870ed467e5c3a06ee620a3a44b9d1
SohamE <soham.584sengupta@gmail.com>
2024-05-05 21:24:21 +0530

    More Improved state management

--- src/TikTacToeComponent.jsx
+++ src/TikTacToeComponent.jsx
@@ -3,24 +3,31 @@ import GameBoard from "./components/Gameboard";
 import Player from "./components/Player";
 import Log from "./components/Log";
 
+const getActivePlayer  = (gameTurns) => {
+    const lastPlayer = gameTurns[0];
+    let currentPlayer = 'O';
+
+    if (lastPlayer && lastPlayer.playerName == 'O') {
+        currentPlayer = 'X';
+    }
+    return currentPlayer;
+}
+
 const TikTacToe = () => {
     // Lift state - as we are using it in children
-    const [playerSymbol, setPlayerSymbol] = useState("O");
+    // const [playerSymbol, setPlayerSymbol] = useState("O");
     const [gameTurns, setGameTurn] = useState([]);
+
+    const currentPlayer = getActivePlayer(gameTurns);
 
     const handleGameTurn = (rowIndex, colIndex) => {
         setGameTurn((prevGameTurns) => {
-            const lastPlayer = prevGameTurns[0];
-            let currentPlayer = 'O';
-
-            if (lastPlayer && lastPlayer.playerName == 'O') {
-                currentPlayer = 'X';
-            }
+            const currentPlayer = getActivePlayer(prevGameTurns);
 
             let newGameTurns = [{ square: { rowIndex, colIndex }, playerName: currentPlayer }, ...prevGameTurns];
             return newGameTurns;
         })
-        setPlayerSymbol((oldPlayerSymbol) => oldPlayerSymbol === 'O' ? 'X' : 'O');
+        // setPlayerSymbol((oldPlayerSymbol) => oldPlayerSymbol === 'O' ? 'X' : 'O');
     }
 
     return (
@@ -29,13 +36,13 @@ const TikTacToe = () => {
             <div id="game-container">
                 <ol>
                     <li>
-                        <Player name="Player 1" symbol="O" isActive={ playerSymbol == 'O' } />
+                        <Player name="Player 1" symbol="O" isActive={ currentPlayer == 'O' } />
                     </li>
                     <li>
-                        <Player name="Player 2" symbol="X" isActive={ playerSymbol == 'X' } />
+                        <Player name="Player 2" symbol="X" isActive={ currentPlayer == 'X' } />
                     </li>
                 </ol>
-                <GameBoard setGameTurn={ handleGameTurn } gameTurns={ gameTurns } playerSymbol={playerSymbol} />
+                <GameBoard setGameTurn={ handleGameTurn } gameTurns={ gameTurns } playerSymbol={currentPlayer} />
             </div>
             <Log gameTurns={gameTurns}/>
         </>
